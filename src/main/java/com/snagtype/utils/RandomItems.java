d0f1537cfdf628bc06d7509fdb761a0763b6bd1f
Way <duyduyyud@yahoo.com>
2020-07-06 00:10:04 -0500

    Finished randomitems with modweight enabled
    createbingo now parses the json file for weighted  items through mods first.

--- src/main/java/com/snagtype/utils/RandomItems.java
+++ src/main/java/com/snagtype/utils/RandomItems.java
@@ -16,6 +16,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import com.google.gson.*;
 
@@ -23,9 +24,10 @@ import com.google.gson.*;
 
 public class RandomItems {
 
-    public File configDirectory = BingoModRandomizor.configDirectory;
+    public static File configDirectory = BingoModRandomizor.configDirectory;
     private static Iterable<Item> items = ForgeRegistries.ITEMS;
     private static List<Item> itemList = Lists.newArrayList( items);
+    private static List<String> modBlackList;
     public static Item getRandomItem() {
 
 
@@ -50,7 +52,7 @@ public class RandomItems {
         return itemList.get(0);
     }
         //now with list parameter for mod weights
-    public static Item getRandomItemFromMod(List<Item> itemListFromMod) {
+    public static Item getRandomItemFromMod(List<Item> itemListFromMod, List <Item> finalList) {
 
 
         Collections.shuffle(itemListFromMod);
@@ -65,13 +67,19 @@ public class RandomItems {
         catch (Exception e) {
             ModBingoLog.info("exception block not found in Creativetabs = " + itemListFromMod.get(0));
             ModBingoLog.info("shuffling items and trying again");
-            return getRandomItemFromMod(itemListFromMod);
+            return getRandomItemFromMod(itemListFromMod, finalList);
         }
         // need to add code for meta values here
 
 
-
-        return itemList.get(0);
+        for (Item name : finalList)// no duplicate items on our bingo card
+        {
+            if (name == itemListFromMod.get(0))
+            {
+                return getRandomItemFromMod(itemListFromMod, finalList);
+            }
+        }
+        return itemListFromMod.get(0);
     }
     public static String getRandomMod(List<String> modList) { // picks a random mod not found on blacklist
 
@@ -86,15 +94,53 @@ public class RandomItems {
          */
         return modList.get(0);
     }
-    public void readJson()
+    public static List <Item> pullItemListFromRandomMod()
     {
-
+        List<String> modList = new ArrayList<>();
         try {
-            Object obj = new JsonParser().parse(new FileReader(configDirectory + "items.json"));
-            JsonObject currMod= (JsonObject) obj;
+            Object obj = new JsonParser().parse(new FileReader(configDirectory + "/items.json"));
+            JsonObject itemsJson= (JsonObject) obj;
+            JsonArray modNamesArray = (JsonArray) itemsJson.get("modnames");
+            //Iterating the contents of the array
+            Iterator<JsonElement> iterator = modNamesArray.iterator();
+
+            while(iterator.hasNext()) {
+                String name = iterator.next().toString();
+                modList.add(name);
+            }
+            for(String blacklist : modBlackList)// removes items from blacklist
+            {
+                for (String entrant : modList)
+                {
+                    if (entrant == blacklist)
+                        modList.remove(entrant);
+                }
+            }
+            Collections.shuffle(modList);
+            String modName = modList.get(0);
+            List <Item>itemListFromMod = new ArrayList<>();
+            ModBingoLog.info(" modname :" + modName.substring(1,modName.length()-1) );
+            JsonObject modObj = itemsJson.getAsJsonObject(modName.substring(1,modName.length()-1));
+            //JsonObject modObj = itemsJson.getAsJsonObject("minecraft");
+            JsonArray itemList = modObj.getAsJsonArray("items");
+            List<String> nameList = new ArrayList<>();
+            iterator = itemList.iterator();
+            while(iterator.hasNext()) {
+                //ModBingoLog.info("iterating item: " + iterator.next());
+                JsonObject item = (JsonObject) iterator.next();
+
+                String name = item.get("name").toString();
+                name = name.substring(1,name.length()-1);
+                //ModBingoLog.info("item name: " + item.get("name").toString());
+                itemListFromMod.add(Item.getByNameOrId(name));
+            }
+            ModBingoLog.info("end iteration, sending itemlist :" + itemListFromMod.toString() );
+            return itemListFromMod;
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
 
@@ -103,28 +149,32 @@ public class RandomItems {
 
         // ** todo going to need to pull the modlist from json file instead here
 
-        //list <String> modList =
         if(numItems > itemList.size() - 1){
             throw new IllegalArgumentException();
         }
 
         // randomly saves i items into a list returns the list
-
+    /*
         for (int i = 1; i < numItems; i++)
             {
                 itemList.add(getRandomItem());
             }
         return itemList.subList(0, numItems);
 
-        /* with mod weights
-        for (int i = 1; i < numItems; i++)
-        {
-            list <item> itemListFromMod = generateModItemList(getRandomMod(modList);
-            getRandomItemFromMod(itemListFromMod)
-        }
-        return itemList.subList(0, numItems);
 
-         */
+     */
+        // with mod weights
+
+         List<Item> itemListFromMod = new ArrayList<>();
+        for (int i = 0; i < numItems; i++)
+        {
+
+            itemListFromMod.add(getRandomItemFromMod(pullItemListFromRandomMod(), itemListFromMod));
+        }
+
+        ModBingoLog.info("itemlist" + itemListFromMod.toString() );
+        return itemListFromMod;
+
     }
 
 }
